df1b242988ab2d3977951b5b01ab01a2f9f15380
andrea bredice <bredice.andrea@gmail.com>
2021-04-24 12:01:11 +0200

    added all pages content + style refactor in css using common classes

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,7 @@
 window.addEventListener('scroll', (event) => {
-    console.log('i did scroll')
-    const width = document.body.Width
-    const heroSection = document.querySelector('.bred-hero')
-    const bredSlogan = document.querySelector('.bred-slogan')
-    heroSection.classList.add('slide-right')
+    // const width = document.body.Width
+    // const heroSection = document.querySelector('.bred-hero')
+    // const bredSlogan = document.querySelector('.bred-slogan')
+    // heroSection.classList.add('slide-right')
     //heroSection[0].style.display = 'none'
 })

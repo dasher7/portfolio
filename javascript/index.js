a172475beb871e3cb2642ade21919d96944360f9
andrea bredice <bredice.andrea@gmail.com>
2021-04-24 13:39:15 +0200

    added basic knowledge to know scroll direction

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,28 @@
+let LAST_SCROLL_Y = 0
+
 window.addEventListener('scroll', (event) => {
+
+    // get all the section in index.html page
+    const allSections = document.getElementsByTagName('section')
+    //console.log('all sections ', allSections)
+
+    // get to know if the scroll event is up or down
+    const userInteraction = event.currentTarget
+
+    console.log(LAST_SCROLL_Y, userInteraction.pageYOffset)
+
+    if (LAST_SCROLL_Y > userInteraction.pageYOffset) { // i am scrolling up
+        console.log('i am scrolling up')
+    }
+    else if (LAST_SCROLL_Y < userInteraction.pageYOffset) { // im am scrolling down
+        console.log('i am scrolling down')
+    } else { // i am doing nothing
+        LAST_SCROLL_Y = 0
+    }
+
+    // set the lAST_SCROLL_Y value
+    LAST_SCROLL_Y = userInteraction.pageYOffset
+
     // const width = document.body.Width
     // const heroSection = document.querySelector('.bred-hero')
     // const bredSlogan = document.querySelector('.bred-slogan')

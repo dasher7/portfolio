a3c5f8b7957454c8709a7286a91fb895d6194a8a
andrea bredice <bredice.andrea@gmail.com>
2021-04-25 10:22:07 +0200

    WIP: added event listener for mobile touchevent in order to get scroll working

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,21 @@
 var CURRENT_SECTION_INDEX = 0;
 let SCROLL_DEBOUNCE = true;
+let INITIAL_TOUCH_Y = 0
 
+/**
+ * START section for scroll/wheel event on desktop
+ */
 window.addEventListener("wheel", (event) => {
 
   if (SCROLL_DEBOUNCE) {
+
     SCROLL_DEBOUNCE = false;
-    //debugger
+
     let hero = document.querySelector('.bred-hero')
     let skills = document.querySelector('.bred-skills')
     let about = document.querySelector('.bred-about')
     let scarlatte = document.querySelector('.bred-scarlatte')
     const allSections = new Array(hero, skills, about, scarlatte)
-    console.log(allSections)
-    // hero.innerHTML = skills.innerHTML
 
     if (event.deltaY < 0) { // scrolling up
 
@@ -39,3 +42,50 @@ window.addEventListener("wheel", (event) => {
 
  }
 });
+
+/**
+ * START section to scroll (touch) on mobile
+ */
+window.addEventListener("touchstart", (event) => {
+  INITIAL_TOUCH_Y = event.changedTouches[0].screenY
+})
+
+window.addEventListener("touchmove", (event) => {
+
+  let hero = document.querySelector('.bred-hero')
+  let skills = document.querySelector('.bred-skills')
+  let about = document.querySelector('.bred-about')
+  let scarlatte = document.querySelector('.bred-scarlatte')
+  const allSections = new Array(hero, skills, about, scarlatte)
+  const actualTouch = event.changedTouches[0].screenY
+
+  if (SCROLL_DEBOUNCE) {
+
+    SCROLL_DEBOUNCE = false;
+
+    if (actualTouch - INITIAL_TOUCH_Y < 0) { // scrolling up
+
+
+      if ( CURRENT_SECTION_INDEX === allSections.length ) return
+      if (CURRENT_SECTION_INDEX + 1 !== allSections.length) {
+        hero.innerHTML = allSections[CURRENT_SECTION_INDEX + 1].innerHTML;
+      }
+      CURRENT_SECTION_INDEX !== allSections.length && (CURRENT_SECTION_INDEX++)
+
+    } else { // scrolling down
+      
+      if ( CURRENT_SECTION_INDEX === 0 ) return
+      if (CURRENT_SECTION_INDEX - 1 <= allSections.length) {
+        hero.innerHTML = allSections[CURRENT_SECTION_INDEX - 1].innerHTML;
+      }
+      CURRENT_SECTION_INDEX !== 0 && (CURRENT_SECTION_INDEX--)
+
+    }
+
+    // change debounce value
+    setTimeout(() => {
+      SCROLL_DEBOUNCE = true;
+    }, 500);
+
+  }
+})
